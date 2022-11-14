996996fae4c84ccf7b392172fbcc331e7068711d
Lucas Maieski <lucasgrigol@gmail.com>
2022-11-14 15:36:54 -0300

    iniciando projeto

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap-utilities.css"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+    <title>Lucas Dev</title>
+
+</head>
+
+<body>
+    <!-- <section class="container topo">
+        <div class="center">
+            <header>
+                <div class="logo">
+                    <img src="images/logo.png" alt="Logo">
+                </div>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </header>
+        </div>
+    </section> -->
+    <header class="topo">
+        <div class="container-md ">
+            <nav class="navbar navbar-expand-md justify-content-bettwen ">
+                <a href="index.php" class="navbar-brand">
+                    <img id="logo" src="images/logo.png" alt="Luks Agency">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav justify-content-end">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contato</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+            <div class="row d-flex align-items-center mt-3 pb-4">
+                <div class="col-12 col-sm-6 time-descricao">
+                    <h2>Melhore a comunicação com seu cliente e time.</h2>
+                    <p>Consultoria especializada em startups, empresase e principalmente pessoas.</p>
+                    <a href="https://lucasgmaieski.github.io/luks-agency/index.html" targer="_blank" >Ver demonstração</a>
+                </div>
+                <div class="col-12 col-sm-6 time-imagem">
+                    <img src="images/ilustracao.png" alt="Ilustração" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </header>
+    <script type="module" src="js/slick.js"></script>
+    <!-- <script src="css/bootstrap/js/bootstrap.js"></script>
+    <script src="css/bootstrap/js/bootstrap.bundle.js"></script> -->
+</body>
+
+
+</html>
