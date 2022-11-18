c0cfac5873bbde90cb0465ed6d74842400f6e2ca
Lucas Maieski <lucasgrigol@gmail.com>
2022-11-18 19:20:26 -0300

    separando footer e header

---
+++ footer.php
@@ -0,0 +1,77 @@
+<footer>
+        <section class="metodologia">
+            <div class="container">
+                <h2>Conheça nossa Metodologia</h2>
+                <p>O que acha de fazermos o que mais gostamos de fazer? Conversar <br> Entre em contato por e-mail ou
+                    telefone.</p>
+                <a href="/contato"> Entrar em contato </a>
+            </div>
+        </section>
+
+        <div class="container">
+            <div class="d-flex flex-row flex-wrap justify-content-between">
+                <div class="contact-footer col-md-6 col-sm-12 row">
+                    <div class="col-4">
+                        <h2>Suporte</h2>
+                        <a href="">Contato</a>
+                        <a href="">FAQ</a>
+                    </div>
+                    <div class="col-4">
+                        <h2>Suporte</h2>
+                        <a href="">Contato</a>
+                        <a href="">FAQ</a>
+                    </div>
+                    <div class="col-4">
+                        <h2>Suporte</h2>
+                        <a href="">Contato</a>
+                        <a href="">FAQ</a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-12 box-logo-footer">
+                    <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/logo-dummy.png" alt="Logo" class="d-inline"></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <?php wp_footer(); ?>
+    <script type="module" src="<?php echo get_template_directory_uri();?>/js/slick.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/js/jquery.js"></script>
+    <script src="<?php echo get_template_directory_uri();?>/js/slick.min.js"></script>
+    <script type="text/javascript">
+    $('.clientes-slider .slider-container').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        centerMode: false,
+        adaptiveHeight: true,
+
+        // centerPadding: 5,
+        speed: 1000,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+
+    $('.depoimentos .depoimentos-box').slick({
+        dots: true,
+        arrows: false,
+        infinite: false,
+        centerMode: false,
+        speed: 1000,
+        slidesToShow: 1,
+    });
+    </script>
+    <!-- <script src="css/bootstrap/js/bootstrap.js"></script>
+    <script src="css/bootstrap/js/bootstrap.bundle.js"></script> -->
+</body>
+
+
+</html>
--- index.php
+++ index.php
@@ -1,58 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/monitor.ico" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+<?php get_header();?>
 
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
-    </script>
-    <title>Lucas Dev</title>
-
-</head>
-
-<body>
-    <header class="topo">
-        <div class="container-md ">
-            <nav class="navbar navbar-expand-md justify-content-bettwen ">
-                <a href="index.php" class="navbar-brand">
-                    <img id="logo" src="<?php echo get_template_directory_uri();?>/images/logo.png" alt="Product Runt">
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="sobre.php">Sobre</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="contato.php">Contato</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </nav>
             <div class="row d-flex align-items-center mt-3 pb-4">
                 <div class="col-12 col-sm-6 time-descricao">
                     <h2>Melhore a comunicação com seu cliente e time.</h2>
@@ -68,6 +16,15 @@
         <div class="circle"><i class="fas fa-angle-down"></i></div>
     </header>
 
+    <?php if(have_posts() ){
+        while( have_posts() ){
+            the_post();
+            the_content();
+        }
+    } else{?>
+        <p>Sorry, no posts matched your criteria.</p>
+    <?php 
+    }?>
     <section class="clientes-slider">
 
         <div class="center">
@@ -163,79 +120,4 @@
         </div>
     </section>
 
-    <footer>
-        <section class="metodologia">
-            <div class="container">
-                <h2>Conheça nossa Metodologia</h2>
-                <p>O que acha de fazermos o que mais gostamos de fazer? Conversar <br> Entre em contato por e-mail ou
-                    telefone.</p>
-                <a href="/contato"> Entrar em contato </a>
-            </div>
-        </section>
-
-        <div class="container">
-            <div class="d-flex flex-row flex-wrap justify-content-between">
-                <div class="contact-footer col-md-6 col-sm-12 row">
-                    <div class="col-4">
-                        <h2>Suporte</h2>
-                        <a href="">Contato</a>
-                        <a href="">FAQ</a>
-                    </div>
-                    <div class="col-4">
-                        <h2>Suporte</h2>
-                        <a href="">Contato</a>
-                        <a href="">FAQ</a>
-                    </div>
-                    <div class="col-4">
-                        <h2>Suporte</h2>
-                        <a href="">Contato</a>
-                        <a href="">FAQ</a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-12 box-logo-footer">
-                    <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/logo-dummy.png" alt="Logo" class="d-inline"></a>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-    <script type="module" src="<?php echo get_template_directory_uri();?>/js/slick.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/jquery.js"></script>
-    <script src="<?php echo get_template_directory_uri();?>/js/slick.min.js"></script>
-    <script type="text/javascript">
-    $('.clientes-slider .slider-container').slick({
-        dots: true,
-        arrows: false,
-        infinite: false,
-        centerMode: false,
-        adaptiveHeight: true,
-
-        // centerPadding: 5,
-        speed: 1000,
-        slidesToShow: 4,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: false,
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 2
-            }
-        }]
-    });
-
-    $('.depoimentos .depoimentos-box').slick({
-        dots: true,
-        arrows: false,
-        infinite: false,
-        centerMode: false,
-        speed: 1000,
-        slidesToShow: 1,
-    });
-    </script>
-    <!-- <script src="css/bootstrap/js/bootstrap.js"></script>
-    <script src="css/bootstrap/js/bootstrap.bundle.js"></script> -->
-</body>
-
-
-</html>
+    <?php get_footer();?>
