d3cac9e638b9784a4b844a82c05caa4424100b92
Lucas Maieski <lucasgrigol@gmail.com>
2022-12-07 22:52:09 -0300

    adicionando função para linkar css e js ao projeto

--- functions.php
+++ functions.php
@@ -9,4 +9,15 @@ function criptografarNome($nome){
     echo "Nome Cripto.: ".$nome2."<br/>";
     echo "Descriptografando nome: ".base64_decode($nome2);
 }
-?>
+
+function pr_theme_styles(){
+    wp_enqueue_style('theme_css', get_template_directory_uri().'/assets/css/style.css');
+    // 3º parametro são as dependendicas para esse arquivo possa funcionar(coisas que o arquivo usa)
+    // depois vem a versão
+    // 5º parametro se for true o arquivo vai ser adicionada só la no final
+    wp_enqueue_script('theme_js', get_template_directory_uri().'/assets/js/script.js', array('jquery'), '1.0', true);
+}
+
+
+//Hooks
+add_action('wp_enqueue_scripts', 'tt_theme_styles');
