9257361ac158fa114ea28cab71315556aee02b76
Lucas Maieski <lucasgrigol@gmail.com>
2022-12-11 20:47:55 -0300

    estilizando página erro 404

--- 404.php
+++ 404.php
@@ -3,7 +3,12 @@
 ?>
 </div>
 </header>
-<h2>Página não encontrada</h2>
+<section class="erro404">
+    <div class="container-img">
+        <img src="" alt="Erro 404">
+    </div>
+    <h2>Página não encontrada</h2>
+</section>
 
 <?php
     get_footer();
